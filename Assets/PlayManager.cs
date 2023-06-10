119bac7f35c22647f5a7dd8f7f3d7ee6c75067f5
ridhobaihaki <hanyatugas9@gmail.com>
2023-06-10 18:24:34 +0700

    Sebuah komit
    - Perubahan pada PlayManager.cs.
    - Melakukan pengaturan dan perubahan pada Finished Canvas.prefab.
    - Melakukan pengaturan dan perubahan pada Level 1.unity.
    - Penambahan Scene Level 2.unity.
    - Pengaturan pada Build Settings.
    - Penambahan-penambahan dan perubahan-perubahan lainnya.

--- Assets/PlayManager.cs
+++ Assets/PlayManager.cs
@@ -1,16 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayManager : MonoBehaviour
 {
+    [SerializeField] GameObject finishedCanvas;
+    [SerializeField] TMP_Text finishedText;
+    int coin = 100; //TODO
     public void GameOver()
     {
-        Debug.Log("GameOver");
+        finishedText.text = "You Failed!";
+        finishedCanvas.SetActive(true);
     }
 
     public void PlayerWin()
     {
-        Debug.Log("PlayerWin");
+        finishedText.text = "You Win!\nScore: " + GetScore();
+        finishedCanvas.SetActive(true);
+    }
+
+    private int GetScore()
+    {
+        return coin * 10;
     }
 }
